1ed509e1a28a604c6ad287b3da807430fe017de2
Saurabh Mishra <11306849+saurabh2mishra@users.noreply.github.com>
2021-03-23 17:38:33 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-EndPointUrl="http://sqs.newspersonalisation.persgroep.cloud:4566"
-QueueUrl= "http://sqs.newspersonalisation.persgroep.cloud:4566/000000000000/hands-on-interview"
+EndPointUrl="http://sqs.test.sample.cloud:4566"
+QueueUrl= "http://sqs.test.sample.cloud:4566/000000000000/"
 logpath="logs"
 outputfileloc = "outputs"
